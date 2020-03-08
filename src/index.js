50812e988966c9fb834d8173153dc10603198fca
bobro <bobrow.bobro2015@yandex.ru>
2020-03-08 19:57:44 +0300

    feat: do all task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var brackets = [];
+  for(var i = 0; i < bracketsConfig.length; i++) {
+  	brackets.push(bracketsConfig[i].join(""));
+  }
+  var arr = str.split("");
+  for(var i = 0; i < arr.length; i++) {
+  	for(var j = 0; j < brackets.length; j++) {
+  		if((arr[i] + arr[i+1]) === brackets[j]) {
+  			arr.splice(i, 2);
+  			i = -1;
+  			j = -1;
+  			continue;
+  		}
+  	}
+  }
+  if(arr.length === 0)
+  	return true;
+  return false;
 }
